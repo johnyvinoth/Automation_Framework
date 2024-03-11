7cecc07c6b08906c63d44cb0e127332a1048f111
johnyvinoth <johnyvinoth@gmail.com>
2024-03-11 12:44:49 +0800

    Issue with iOS suite fixed

--- Web_UI_Selenium_Framework/src/test/java/webpage_Test.java
+++ Web_UI_Selenium_Framework/src/test/java/webpage_Test.java
@@ -1,12 +1,16 @@
 import io.qameta.allure.Description;
 import io.qameta.allure.Step;
 import org.automation.WebUITestBase;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.net.URL;
+
 public class webpage_Test extends WebUITestBase {
     @Test(enabled = true)
     @Step("Test Step")
@@ -46,6 +50,25 @@ public class webpage_Test extends WebUITestBase {
         Thread.sleep(2000);
         logger.info("Safari browser Test successful");
         safariDriver.quit();
+
+    }
+
+//    Question asked many times in Hackkerrank online test.
+    public void TestHackkerrank(WebDriver driver, String pageUrl)
+    {
+// Navigate to the given URL
+        driver.get(pageUrl);
+// Find the input element and enter the value.
+
+        driver.findElement(By.id("fname")).sendKeys("Fizz");
+        driver.findElement(By.id("lname")).sendKeys("Bizz");
+        driver.findElement(By.id("email")).sendKeys("hackerrank@hackerrank.com");
+        driver.findElement(By.id("password")).sendKeys("lemetin@Hrw");
+        driver.findElement(By.id("c_password")).sendKeys("c_lemetin@Hrw");
+
+
+//        Click the submit button
+        driver.findElement(By.xpath("//button[text()='Register'])")).click();
 
     }
 
