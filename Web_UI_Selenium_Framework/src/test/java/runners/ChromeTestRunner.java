c53183fc04b0abfbee5149b38333bdcbca8bce15
johnyvinoth <johnyvinoth@gmail.com>
2024-04-04 13:57:30 +0800

    Changed the padletTestSiteRunner to be able to run through Testng
    Changed the padletTestSiteRunner to be able to run through Testng

--- Web_UI_Selenium_Framework/src/test/java/runners/ChromeTestRunner.java
+++ Web_UI_Selenium_Framework/src/test/java/runners/ChromeTestRunner.java
@@ -1,14 +1,11 @@
 package runners;
 
-//import io.cucumber.junit.CucumberOptions;
-
 import io.cucumber.testng.AbstractTestNGCucumberTests;
-//import cucumber.api.testng;
 import io.cucumber.testng.CucumberOptions;
 import org.Utils.WebDriverFactory;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
-//import cucumber.api.CucumberOptions;
+
 
 //@RunWith(Cucumber.class)
 @CucumberOptions(
--- Web_UI_Selenium_Framework/src/test/java/runners/padletTestSiteRunner.java
+++ Web_UI_Selenium_Framework/src/test/java/runners/padletTestSiteRunner.java
@@ -1,10 +1,14 @@
 package runners;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.Utils.WebDriverFactory;
+import org.junit.runner.RunWith;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+//@RunWith(Cucumber.class)
 
 @CucumberOptions(
         features = {"src/test/Features/padletTestSite.feature"}
@@ -14,6 +18,17 @@ import org.junit.runner.RunWith;
 
 )
 
-public class padletTestSiteRunner {
+public class padletTestSiteRunner extends AbstractTestNGCucumberTests {
+    @BeforeSuite
+    public void setup() {
+        System.out.println("Setup starting");
+    }
+
+
+    @AfterSuite
+    public void teardown() {
+        WebDriverFactory.quitWebDriver();
+        System.out.println("Tear down completed");
+    }
 
 }
